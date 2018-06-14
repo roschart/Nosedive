63089459715c28ca3fef765a8ad606da4f076c28
Jose Luis Dominguez Balirac <jl.dominguez@Payvision.local>
2018-06-14 17:36:37 +0200

    Default lein app product

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to nosedive
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/nosedive/core_test.clj
@@ -0,0 +1,7 @@
+(ns nosedive.core-test
+  (:require [clojure.test :refer :all]
+            [nosedive.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
