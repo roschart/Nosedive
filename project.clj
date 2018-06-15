94804f349c88cd2fad5799b019641fdc2197d4d7
Jose Luis Domínguez Balirac <jl.balirac@payvision.com>
2018-06-15 15:00:44 +0200

    RED: Creation of the database without checking

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/java.jdbc "0.7.6"]
+                 [org.xerial/sqlite-jdbc "3.23.1"]]
   :main ^:skip-aot nosedive.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :plugins [[lein-ancient "0.6.15"]])
--- src/nosedive/core.clj
+++ src/nosedive/core.clj
@@ -1,7 +1,39 @@
 (ns nosedive.core
+  (:require [clojure.java.jdbc :refer :all])
   (:gen-class))
 
+(def testdata
+  {:date "2018-96-12",
+   :creator "jl.balirac@payvision.com",
+   :person "c.cobo@payvision.com",
+   :vote 5,
+   :description "Me invitó a un pincho de tortilla"})
+
+
+(def db
+  {:classname   "org.sqlite.JDBC"
+   :subprotocol "sqlite"
+   :subname     "db/database.db"})
+
+
+(defn create-db []
+  (try (db-do-commands db
+                       (create-table-ddl :votes
+                                         [[:date :text]
+                                          [:creator :text]
+                                          [:person :text]
+                                          [:vote :number]
+                                          [:description :text]]))
+       (catch Exception e (println e))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (create-db)
+  (insert! db :votes testdata)
+
+  (def output
+    (query db "select * from votes"))
+
+  (keys (first output))
+  (:body (first output)))
