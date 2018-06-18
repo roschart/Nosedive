cb6aa1252842361929c0ba6aa0feecdaa7387bcd
Jose Luis Domínguez Balirac <jl.balirac@payvision.com>
2018-06-18 15:32:19 +0200

    Refactor: Move functions to new namespace parse-options

--- src/nosedive/core.clj
+++ src/nosedive/core.clj
@@ -1,6 +1,7 @@
 (ns nosedive.core
   (:require [clojure.java.jdbc :refer :all]
-            [clojure.tools.cli :refer [parse-opts]]
+            ; [clojure.tools.cli :refer [parse-opts]]
+            [nosedive.parse-options :refer :all]
             [either.core :as e])
   (:gen-class))
 
@@ -17,49 +18,13 @@
    :subprotocol "sqlite"
    :subname     "db/database.db"})
 
-(def required-opts #{:creator :description :person :vote})
-
-(defn missing-required?
-  "Returns true if opts is missing any of the required-opts"
-  [opts]
-  (not-every? opts required-opts))
-
-
-(def cli-options
-  ;; An option with a required argument
-  [["-d" "--date DATE" "Date ddMMyyyy"
-    :default (java.util.Date.)
-    :parse-fn #(.parse  (java.text.SimpleDateFormat. "ddMMyyyy") %)]
-   ;; A non-idempotent option
-   ["-c" "--creator  WHO" "Who vote"]
-   ["-D" "--description  DESCRIPTION" "The descirption"]
-   ["-p" "--person PERSON" "The person that is voted"]
-   ["-v" "--vote vote" "The vote"
-    :parse-fn #(Integer/parseInt %)
-    :validate [#(< 0 % 6) "Must be a number between 1 and 5"]]
-   ["-h" "--help"]])
-
-(defn check-errors [options]
-  (if (:errors options)
-    {:status :error :result (:errors options)}
-    {:status :success :result options}))
-
-(defn check-missing [options]
-  (if (missing-required? (:options options))
-    {:status :error :result (:summary options)}
-    {:status :success :result options}))
-
-(defn check-help [options]
-  (if (get-in options [:options :help])
-    {:status :error :result (:summary options)}
-    {:status :success :result options}))
 
 (defn process [data]
   (println (str "Los datos a procesar son:" data)))
 
 (defn -main
   [& args]
-  (let [{:keys [options arguments summary errors] :as po}  (parse-opts args cli-options)]
+  (let [{:keys [options arguments summary errors] :as po}  (parse-opts args)]
     (-> po
         check-errors
         (e/chain check-missing)
---
+++ src/nosedive/parse_options.clj
@@ -0,0 +1,42 @@
+(ns nosedive.parse-options
+  (:require [clojure.tools.cli :refer [parse-opts] :rename {parse-opts po}]))
+
+(def required-opts #{:creator :description :person :vote})
+
+(defn missing-required?
+  "Returns true if opts is missing any of the required-opts"
+  [opts]
+  (not-every? opts required-opts))
+
+
+(def cli-options
+  ;; An option with a required argument
+  [["-d" "--date DATE" "Date ddMMyyyy"
+    :default (java.util.Date.)
+    :parse-fn #(.parse  (java.text.SimpleDateFormat. "ddMMyyyy") %)]
+   ;; A non-idempotent option
+   ["-c" "--creator  WHO" "Who vote"]
+   ["-D" "--description  DESCRIPTION" "The descirption"]
+   ["-p" "--person PERSON" "The person that is voted"]
+   ["-v" "--vote vote" "The vote"
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 6) "Must be a number between 1 and 5"]]
+   ["-h" "--help"]])
+
+(defn check-errors [options]
+  (if (:errors options)
+    {:status :error :result (:errors options)}
+    {:status :success :result options}))
+
+(defn check-missing [options]
+  (if (missing-required? (:options options))
+    {:status :error :result (:summary options)}
+    {:status :success :result options}))
+
+(defn check-help [options]
+  (if (get-in options [:options :help])
+    {:status :error :result (:summary options)}
+    {:status :success :result options}))
+(defn parse-opts [args]
+  (po args cli-options))
+
